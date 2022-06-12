2f46dbd157da00cae89d22b82f1a60a7c51fb111
YenSea <nagachethann@gmail.com>
2022-06-12 09:39:42 +0530

    Basic Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# RetroInMetro_Web
-This repo is intended to use to store and work on Web related resources of Retro In Metro website
---
+++ home.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>RetroInMetro-Home</title>
+
+        <!-- swiper css link -->
+        <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css"/>
+
+        <!-- font awesome cdn link -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+        <!-- custom css file link -->
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+
+    <!-- header section starts -->
+    <section class="header">
+        <a href="home.php" class="logo">.RetroInMetro</a>
+
+        <nav class="navbar">
+            <a href="home.php">Home</a>
+            <a href="vision.php">Vision</a>
+            <a href="products.php">Products</a>
+            <a href="contactus.php">Contact us</a>
+        </nav>
+
+        <div id="menu-btn" class="fas fa-bars"></div>
+    </section>
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- footer section starts -->
+    <section class='footer'>
+        <div class="box-container">
+            <div class="box">
+                <h3>quick links</h3>
+                    <a href="home.php"><i class="fas fa-angle-right"></i>Home</a>
+                    <a href="vision.php"><i class="fas fa-angle-right"></i>Vision</a>
+                    <a href="products.php"><i class="fas fa-angle-right"></i>Products</a>
+                    <a href="contactus.php"><i class="fas fa-angle-right"></i>Contact us</a>
+            </div>
+            <div class="box">
+                <h3>extra Links</h3>
+                    <a href="#"><i class="fas fa-angle-right"></i>ask questions</a>
+                    <a href="#"><i class="fas fa-angle-right"></i>about us</a>
+                    <a href="#"><i class="fas fa-angle-right"></i>privacy policy</a>
+                    <a href="#"><i class="fas fa-angle-right"></i>terms of use</a>
+            </div>
+            <div class="box">
+                <h3>contact info</h3>
+                    <a href="#"><i class="fas fa-phone"></i>ask questions</a>
+            </div>
+        </div>
+    </section>
+    <!-- footer section ends -->
+
+
+
+
+
+
+
+    
+    <!-- swiper js link -->
+    <script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
+
+    <!-- custom js file link -->
+    <script src="js/script.js"></script>
+    </body>
+</html>
